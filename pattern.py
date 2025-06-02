dfbe9ff7dba9165723a3f75a80321b43e2ae826d
my name <me@example.com>
2025-06-02 19:35:05 +0530

    first commit in test branch

--- pattern.py
+++ pattern.py
@@ -10,3 +10,28 @@ def star_pattern(n):
     print("\n--- 1. Star Pattern ---")
     for i in range(1, n + 1):
         print("*" * i)
+ def half_pyramid_number_pattern(n):
+    """
+    Prints a right-angled triangle number pattern.
+
+    Args:
+        n (int): The number of rows in the pattern.
+    """
+    print("\n--- 3. Half Pyramid Number Pattern ---")
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end="")
+        print()
+
+def inverted_pyramid_number_pattern(n):
+    """
+    Prints an inverted right-angled triangle number pattern.
+
+    Args:
+        n (int): The number of rows in the pattern.
+    """
+    print("\n--- 4. Inverted Pyramid Number Pattern ---")
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print(j, end="")
+        print()
